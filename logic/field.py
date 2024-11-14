9b6b3589d0e535577c61fc7e245d1202ae65b2da
Мудрый Дуб Тимлидович <pirumovgeorgij@gmail.com>
2024-11-14 14:15:53 +0300

    task 9 is done, Field class is made

---
+++ logic/field.py
@@ -0,0 +1,17 @@
+from objects.base import BaseObject
+
+
+class Field(BaseObject):
+    def __init__(self, path_to_file = 'field.txt'):
+        self.l=[] # создаём двумерный массив который соответствует файлу field.txt
+        f = open(path_to_file)
+        f = [str(i) for i in f.readlines()]
+        for i in range(len(f)):
+            self.l.append([])
+            for j in f[i]:
+                if j!='\n':
+                    self.l[i].append(j)
+
+
+
+
