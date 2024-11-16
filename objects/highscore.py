d57b064d180b6544f79a8b22e8635d6b320d8e38
vanya <idub3681@gmail.com>
2024-11-16 16:22:37 +0300

    images

--- objects/highscore.py
+++ objects/highscore.py
@@ -1,5 +1,10 @@
+import pyray as pr
 class HighscoreTableDrawer:
     def __init__(self, highscore_data):
         self.highscore_data = highscore_data
     def draw_table(self):
-        pass
+        title_font_size = 30
+        table_x = 50
+        table_y = 20
+        
+        pr.draw_text("Highscore Table", table_x, table_y, title_font_size, pr.WHITE)
