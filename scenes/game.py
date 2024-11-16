be6678fec4eb4a023fdd949163e52afcdcf1d163
Мудрый Дуб Тимлидович <pirumovgeorgij@gmail.com>
2024-11-16 14:45:12 +0300

    task 15 is done, Cherry class is made

--- scenes/game.py
+++ scenes/game.py
@@ -9,6 +9,7 @@ from objects.ghosts.blinky import BlinkyGhost
 from objects.ghosts.pinky import PinkyGhost
 from objects.ghosts.inky import InkyGhost
 from objects.ghosts.clyde import ClydeGhost
+from objects.cherry import Cherry
 
 class GameScene(BaseScene):
     def __init__(self):
@@ -20,6 +21,7 @@ class GameScene(BaseScene):
         self.pinkGhost = PinkyGhost(200, 200, 14)
         self.inkyGhost = InkyGhost(300, 300, 12)
         self.clydeGhost = ClydeGhost(400, 400, 11)
+        self.cherry = Cherry(300, 300)
         super().__init__()
 
     #def set_up_objects(self):
@@ -38,6 +40,8 @@ class GameScene(BaseScene):
         self.inkyGhost.draw()
         self.clydeGhost.update()
         self.clydeGhost.draw()
+        self.cherry.update()
+        self.cherry.draw()
         if pyray.is_key_pressed(pyray.KeyboardKey.KEY_P):
             Settings.set_scene(1)
         elif pyray.is_key_pressed(pyray.KeyboardKey.KEY_ONE):
