b86411921a595a68ed2d27b6fce9429793b98f36
Pasha <pirumov.pavel@gmail.com>
2024-11-21 18:00:29 +0300

    pacman collision

--- objects/images/pacman.py
+++ objects/images/pacman.py
@@ -19,14 +19,28 @@ class Pacman(BaseObject):
     def update(self):
         self.cellx = self.x//80
         self.celly = self.y // 80
-        print(Field_structure[self.celly][self.cellx])
+        #print(Field_structure[self.celly][self.cellx], self.y, self.celly)
         # Обновление положения пакмана в зависимости от направления
         if self.direction == "UP":
-            self.y -= self.speed
+            if int(Field_structure[self.celly-1][self.cellx]) == 1 or (int(Field_structure[self.celly-1][self.cellx+1]) == 1 and self.x!=(self.cellx+1)*80-80):
+                if self.y == (self.celly-1) * 80+80:
+                    self.y = self.y
+                else:
+                    self.y -= self.speed
+
+            else:
+                self.y -= self.speed
         elif self.direction == "DOWN":
-            self.y += self.speed
+            if int(Field_structure[self.celly+1][self.cellx]) == 1 or (int(Field_structure[self.celly+1][self.cellx+1]) == 1 and self.x!=(self.cellx+1)*80-80):
+                #if self.y == (self.celly+1) * 80+80:
+                self.y = self.y
+                #else:
+                    #self.y += self.speed
+
+            else:
+                self.y += self.speed
         elif self.direction == "LEFT":
-            if int(Field_structure[self.celly][self.cellx-1]) == 1:
+            if int(Field_structure[self.celly][self.cellx-1]) == 1 or (int(Field_structure[self.celly+1][self.cellx-1]) == 1 and self.y!=(self.celly+1)*80-80):
                 if self.x==(self.cellx-1)*80+80:
                     self.x = self.x
                 else:
@@ -34,7 +48,7 @@ class Pacman(BaseObject):
             else:
                 self.x -= self.speed
         elif self.direction == "RIGHT":
-            if int(Field_structure[self.celly][self.cellx+1]) == 1:
+            if int(Field_structure[self.celly][self.cellx+1]) == 1 or (int(Field_structure[self.celly+1][self.cellx+1]) == 1 and self.y!=(self.celly+1)*80-80):
                 self.x = self.cellx*80
             else:
                 self.x += self.speed
@@ -53,12 +67,16 @@ class Pacman(BaseObject):
         pyray.draw_rectangle(self.x, self.y, self.width, self.height, pyray.YELLOW)
     def game(self):
         if pyray.is_key_down(pyray.KEY_W):
+            #if int(Field_structure[self.celly - 1][self.cellx]) != 1:
             self.set_direction("UP")
         elif pyray.is_key_down(pyray.KEY_S):
+            #if int(Field_structure[self.celly + 1][self.cellx]) != 1:
             self.set_direction("DOWN")
         elif pyray.is_key_down(pyray.KEY_A):
+            #if int(Field_structure[self.celly][self.cellx - 1]) != 1:
             self.set_direction("LEFT")
         elif pyray.is_key_down(pyray.KEY_D):
+            #if int(Field_structure[self.celly][self.cellx + 1]) != 1:
             self.set_direction("RIGHT")
         self.update()
     def set_direction(self, direction):
