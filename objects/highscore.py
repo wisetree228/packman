b626deda1a66e270d4f8bae7bec1ba81094be213
vanya <idub3681@gmail.com>
2024-11-30 15:01:52 +0300

    highscore draw

--- objects/highscore.py
+++ objects/highscore.py
@@ -1,10 +1,14 @@
 import pyray as pr
 class HighscoreTableDrawer:
-    def __init__(self, highscore_data):
+    def __init__(self, highscore_data, text = "Table",table_x:int = 100 ,table_y:int = 150, tittle_font_size:int = 30):
         self.highscore_data = highscore_data
+        self.text = text
+        self.table_x = table_x
+        self.table_y = table_y
+        self.tittle_font_size = tittle_font_size
     def draw_table(self):
-        title_font_size = 30
-        table_x = 50
-        table_y = 20
-        
-        pr.draw_text("Highscore Table", table_x, table_y, title_font_size, pr.WHITE)
+        pr.begin_drawing()
+        pr.draw_text(self.text , self.table_x, self.table_y, self.tittle_font_size, pr.WHITE)
+        pr.end_drawing()
+        for i in range(len(self.highscore_data)):
+            pr.draw_text(f'player: {self.highscore_data[i]["name"]}, record: {self.highscore_data[i]["score"]}', 50, 200+(i*30), 25, pr.WHITE)
