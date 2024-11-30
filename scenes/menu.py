1eb7d672bc1ad789d886c6a0855afa31e2775591
vanya <idub3681@gmail.com>
2024-11-30 15:12:52 +0300

    highscore table draw

--- scenes/menu.py
+++ scenes/menu.py
@@ -1,5 +1,6 @@
 import pyray
 import raylib
+from objects.highscore import HighscoreTableDrawer
 
 from objects.text import Text
 from scenes.base import BaseScene
@@ -17,6 +18,16 @@ class MenuScene(BaseScene):
         self.cell2 = Cell(80, 0, 1)
         self.cell3 = Cell(160, 0, 2)
         self.cell4 = Cell(240, 0, 3)
+        self.highscoredrawer = HighscoreTableDrawer(highscore_data=[ {'name':'vanya', 'score':'100000000'},
+                                                                     {'name':'player2', 'score':'12334'},
+                                                                     {'name':'player3', 'score':'1111'},
+                                                                     {'name':'player4', 'score':'900'},
+                                                                     {'name':'player5', 'score':'800'},
+                                                                     {'name':'player6', 'score':'700'},
+                                                                     {'name':'player7', 'score':'600'},
+                                                                     {'name':'player8', 'score':'500'},
+                                                                     {'name':'player9', 'score':'225'},
+                                                                     {'name':'player10', 'score':'12'}])
         #self.new_game_text = Text(Settings.WIDTH // 2 - 70, Settings.HEIGHT // 2 - 100, "New game", 40)
         #self.exit_text = Text(Settings.WIDTH // 2 - 20, Settings.HEIGHT // 2, "Exit", 40)
         super().__init__()
@@ -33,6 +44,7 @@ class MenuScene(BaseScene):
         #self.objects.append(self.exit_text)
     def additional_process_event(self):
         self.pacman.game()
+        self.highscoredrawer.draw_table()
         if self.exit_button.check_click():
             pyray.close_window()
         if self.new_game_button.check_click():
